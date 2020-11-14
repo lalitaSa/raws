a7d54dbb23056470faaace39a87aceca311ea279
lalitaSa <plaboo.guy@gmail.com>
2020-11-15 00:23:48 +0700

    Update models.py

--- Mysite/Myapp/models.py
+++ Mysite/Myapp/models.py
@@ -4,9 +4,12 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=30)
     phone= models.CharField(max_length=10)
+    email = models.EmailField(max_length=254)
+    dob = models.DateField(max_length=8)
     GENDER_CHOICES = (
         ('M', 'Male'),
         ('F', 'Female'),
     )
     gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
+    school = models.CharField(max_length=50)
     cus_img = models.ImageField(upload_to='img')
